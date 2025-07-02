90da685d507eb486bca0979ee6d04d0decf5ef75
nildo-nil <rgfl7405@gmail.com>
2025-07-02 21:37:40 +0700

    add
    add

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿public class program
+﻿public class Program
 {
-    public static void main(String[] args)
+    public static void Main(String[] args)
     {
-        
+        Console.WriteLine("Hello World");
+        Console.Write("Hello");
+        Console.WriteLine("World");
+        Console.WriteLine("Hay nhap mot so tren ban phim");
+        int so = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("" + so);
+        Console.ReadLine();//doc du lieu tu ban phim
     }
 
 }
